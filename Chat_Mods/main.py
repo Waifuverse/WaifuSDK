145f08887908b17acd71d2f68da09f6a57291519
Baca, David <dbaca6@gatech.edu>
2023-04-14 12:26:14 -0700

    chatmod chhanges
    added functions to the chatmods.
    added function to only return quoted text

--- Chat_Mods/main.py
+++ Chat_Mods/main.py
@@ -9,47 +9,45 @@ KITSUNE_SERVER_PORT = 8765
 MACAQUE_SERVER_ADDRESS = "localhost"
 MACAQUE_SERVER_PORT = 8000
 
+clients = {"Kitsune": None, "Macaque": None}
+
 async def handle_client(websocket, path, client_type):
+    global clients
+
     print(f"Client connected: {websocket.remote_address} ({client_type})")
+    clients[client_type] = websocket
+
     while True:
         input = await websocket.recv()
-
-        #the input you receive is a
         print(f"Received message from {client_type}: {input}")
 
-
-        # Perform different actions based on client type
         if client_type == "Kitsune":
-            # Handle Kitsune client input
-            output = "Kitsune output"
+            print("sending to Macaque")
+            output = input
+            if clients["Macaque"]:
+                await clients["Macaque"].send(output)
         elif client_type == "Macaque":
-            # Handle Macaque client input
-            output = "Macaque output"
             msg = json.loads(input)
-
             isGenerating = bool(msg['isGenerating'])
-            #print(isGenerating)
             output = remove_extra_new_lines(msg['content'])
-            if not isGenerating:
+            #remove the RP ** lines
+            output = extract_text_between_quotes(output)
 
-                print("sending to kitsune")
-                print(output)
+            if not isGenerating:
+                if clients["Kitsune"]:
+                    await clients["Kitsune"].send(output)
+                    print(f"Output sent to Kitsune: {output}")
             else:
+                print("output generating")
                 print(output)
         else:
-            # Handle other client types
             output = "Unknown client type"
 
-        #await websocket.send(output)
-
-
 async def start_server():
-    # Start the server for Kitsune clients
     kitsune_server = await websockets.serve(lambda websocket, path: handle_client(websocket, path, "Kitsune"), KITSUNE_SERVER_ADDRESS, KITSUNE_SERVER_PORT)
     kitsune_address = kitsune_server.sockets[0].getsockname()
     print(f"Kitsune server started at ws://{kitsune_address[0]}:{kitsune_address[1]}")
 
-    # Start the server for Macaque clients
     macaque_server = await websockets.serve(lambda websocket, path: handle_client(websocket, path, "Macaque"), MACAQUE_SERVER_ADDRESS, MACAQUE_SERVER_PORT)
     macaque_address = macaque_server.sockets[0].getsockname()
     print(f"Macaque server started at ws://{macaque_address[0]}:{macaque_address[1]}")
@@ -58,21 +56,18 @@ async def start_server():
 
 import re
 def remove_extra_new_lines(input_string):
-
-
     input_string = input_string.encode('ascii', 'ignore').decode('ascii')
-
-    # Remove non-printable characters
     input_string = ''.join(filter(lambda x: x.isprintable(), input_string))
-
-    # Remove extra new lines
     input_string = re.sub(r'\n+', '\n', input_string)
-
-    # Remove leading and trailing white space
     input_string = input_string.strip()
 
     return input_string
+def extract_text_between_quotes(text):
+    if '"' not in text:
+        return text
 
-
+    quoted_text = re.findall(r'"([^"]*)"', text)
+    result = ' '.join(quoted_text)
+    return result
 if __name__ == "__main__":
     asyncio.run(start_server())
